2fc0cbca5832e3bb9a02d7258326dd1f1cd345ef
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-03 23:58:15 -0600

    Create Info.tsx

---
+++ components/Info.tsx
@@ -0,0 +1,19 @@
+
+const Info = ({ }) => {
+    return (
+        <div className="abajo">
+            <div className="col-md-12 mt-4">
+                <div className="text-center">
+
+                    <h1 className="btn btn-outline-light">Gmail: sotopauu157@gamil.com</h1>
+                    <h1 className="btn btn-outline-light">Paula.Soto.Morales Phone:(85089343)</h1>
+                    <h1 className="btn btn-outline-light">Address: 800 meters south west of Ojo de Agua school</h1>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Info
