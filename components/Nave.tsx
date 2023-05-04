ee74bc8172a54f2e5629198b28d70336f8f3f6e4
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-03 23:57:40 -0600

    Create Nave.tsx

---
+++ components/Nave.tsx
@@ -0,0 +1,34 @@
+
+import Link from "next/link";
+const Nave = ({ }) => {
+  return (
+
+
+    <>
+
+      <h1 className="title">Project Information Section</h1>
+      <img className="images" src="/git.png" alt="Not Found" />
+
+      <a href="#" className="button"></a>
+      <h1>Each project has a reference link to access Github
+      </h1>
+
+      <h1 className="text1">Asignacion1</h1>
+      <p>  More information in the next link:<a className="linkedin" href="https://github.com/PaulaAlexiaSotoMorales/Asignacion1"> ----Click me to go to the project----</a></p>
+
+      <h1 className="text1">Asignacion2 <p>More information in the next link: <a className="linkedin" href="https://github.com/PaulaAlexiaSotoMorales/Asignacion2"> ----Click me to go to the project----</a></p></h1>
+
+
+      <h2 className="text1">React-Challangues <p>More information in the next linkk: <a className="linkedin" href="https://github.com/PaulaAlexiaSotoMorales/React-Challangues"> ----Click me to go to the project----</a></p></h2>
+
+      <h1 className="text1">Tarea2 <p>More information in the next link: <a className="linkedin" href="https://github.com/PaulaAlexiaSotoMorales/Tarea2"> ----Click me para ir al proyecto----</a></p></h1>
+
+
+      <h1 className="text1">Tarea Programada  <p>More information in the next link:<a className="linkedin" href="https://github.com/PaulaAlexiaSotoMorales/TareaProgramada"> ----Click me to go to the project----</a></p></h1>
+
+
+    </>
+
+  )
+}
+export default Nave
