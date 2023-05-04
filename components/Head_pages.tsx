3b9f7203c6f3f7f2feec9a8cb661d63ba5f55603
PaulaAlexiaSotoMorales <130626285+PaulaAlexiaSotoMorales@users.noreply.github.com>
2023-05-03 23:56:44 -0600

    Create Head_pages.tsx

---
+++ components/Head_pages.tsx
@@ -0,0 +1,31 @@
+const PerfilU = ({ }) => {
+    return (
+
+
+
+        <div className="perr">
+            <h1 className="title">Personal information</h1>
+            <img className="images" src="/download (1).png" alt="Not Found" />
+
+
+            <h1>Paula Alexia Soto Morales</h1>
+            <h1>Information Systems Engineer</h1>
+
+            <h1>
+                I am a programmer with experience in MySQL,C++,Javasricp, php,
+                During 4 years I have programmed Internet pages using different frameworks.
+
+                I feel qualified to apply for the job available at Empresa Actualidad.
+                The challenges and tasks that I have to face in your institution will be
+                completed professionally and with the greatest possible efficiency.
+
+
+            </h1>
+
+            <a className="git" href="www.linkedin.com/in/paula-alexia-soto-morales-bbb801274">----Click me para poder ver el perfil de linkeld----</a>
+
+        </div>
+
+    )
+}
+export default PerfilU
